4038720ce8d4e1ce19f0996fe501e0dc1b2501cb
Luca Valnegri <l.valnegri@datamaps.co.uk>
2022-12-19 02:50:03 +0000

    conversion crs ons weighted

--- data-raw/include_datasets.R
+++ data-raw/include_datasets.R
@@ -12,7 +12,7 @@ y <- qs::qread(file.path(Rfuns::bnduk_path, 's20', 'MSOA21')) |> setnames('MSOA2
 bbx <- as.numeric(sf::st_bbox(y))
 assign(fn, y)
 save( list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip' )
-sf::st_write(y, './data-raw/shp/MSOA.shp')
+sf::st_write(y, './data-raw/shp/MSOA.shp', append=FALSE)
 
 # initial map
 fn <- 'mps'
@@ -35,7 +35,7 @@ fn <- 'MSOAgb'
 y <- qs::qread(file.path(Rfuns::bnduk_path, 's00', 'MSOA21gb')) |> setnames('MSOA21', 'MSOA')
 assign(fn, y)
 save( list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip' )
-sf::st_write(y, './data-raw/shp/MSOAgb.shp')
+sf::st_write(y, './data-raw/shp/MSOAgb.shp', append=FALSE)
 
 # MSOA data.table
 fn <- 'zones'
@@ -43,14 +43,17 @@ y1 <- data.table(
           y |> st_drop_geometry(), 
           area = y |> st_area() |> as.integer(),
           perimeter = y |> lwgeom::st_perimeter() |> as.integer(),
-          y |> st_centroid() |> st_geometry() |> st_transform(4326) |> st_coordinates() |> as.data.table() |> setNames(c('x_lon', 'y_lat'))
+          y |> st_centroid() |> st_geometry() |> st_transform(4326) |> st_coordinates() |> as.data.table() |> setNames(c('gx_lon', 'gy_lat'))
       ) |> setkey('MSOA')
 # >>>>>> this should be corrected when ONS publishes its very own Census 2021 weighted centroids <<<<<<<
 y2 <- fread(
           'https://opendata.arcgis.com/api/v3/datasets/c0e3f920e20e41b3a994dd7fc7333c91_0/downloads/data?format=csv&spatialRefId=27700&where=1%3D1', 
           select = c('msoa11cd', 'X', 'Y'), 
-          col.names = c('MSOA', 'ox_lon', 'oy_lat')
-      ) |> setkey('MSOA')
+          col.names = c('MSOA', 'x', 'y')
+      ) |>
+      st_as_sf(coords = c('x', 'y'), crs = 27700) |> 
+      st_transform(4326) 
+y2 <- data.table(y2 |> st_drop_geometry(), y2 |> st_coordinates()) |> setkey('MSOA') |> setNames(c('MSOA', 'ox_lon', 'oy_lat'))
 y3 <- fst::read_fst(file.path(Rfuns::datauk_path, 'fb_gridpop', 'total')) |> 
           wcentroids(27700, from_wgs = TRUE, id = 'MSOA') |> 
           setkey('MSOA')
@@ -84,3 +87,7 @@ fn <- 'msoa.lst'
 y <- Rfuns::create_list(y[, .(MSOA, MSOAd)][order(MSOAd)], FALSE) 
 assign(fn, y)
 save( list = fn, file = file.path('data', paste0(fn, '.rda')), version = 3, compress = 'gzip' )
+
+# clean
+rm(list = ls())
+gc()
